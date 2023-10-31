ace09fc1d39fc1b7bbab45f9e14bc3ceefb69c15
Vitor César <65216891+lordvitor11@users.noreply.github.com>
2023-10-31 08:40:52 -0300

    Delete insert.php

--- insert.php
+++
@@ -1,6 +0,0 @@
-<?php session_start();
-    $_SESSION['username'] = isset($_POST['user']) ? $_POST['user'] : $_GET['user'];
-    $_SESSION['logged_in'] = true;
-
-    header("Location: index.php");
-?>
